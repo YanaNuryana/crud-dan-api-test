4f6f83ce1e35d1fe5f4c6842959e5d42faa5c395
Yana Nuryana <yana.nuryana@edi-indonesia.co.id>
2025-02-19 14:28:12 +0700

    Initial commit

--- src/main/java/com/technical_test/technical_test/controller/DataUserController.java
+++ src/main/java/com/technical_test/technical_test/controller/DataUserController.java
@@ -1,13 +1,11 @@
 package com.technical_test.technical_test.controller;
 
 import com.technical_test.technical_test.dto.DataUserDTO;
-import com.technical_test.technical_test.model.DataUser;
 import com.technical_test.technical_test.service.DataUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/data_user")
@@ -18,19 +16,21 @@ public class DataUserController {
 
     @PostMapping("/create")
     public DataUserDTO create(@RequestBody DataUserDTO request) {
-        final DataUser dataUser = dataUserService.mapToEntity(request);
-        final DataUser result = dataUserService.create(dataUser);
-        return dataUserService.mapToDto(result);
-
+        return dataUserService.create(request);
     }
 
     @GetMapping("/all")
-    public List<DataUserDTO> findAll(){
-        return dataUserService.findAll().stream().map(dataUser -> dataUserService.mapToDto(dataUser)).collect(Collectors.toList());
+    public List<DataUserDTO> findAll() {
+        return dataUserService.findAll();
+    }
+
+    @PutMapping("/{userId}")
+    public DataUserDTO updateUser(@PathVariable Integer userId, @RequestBody DataUserDTO dataUserDTO) {
+        return dataUserService.update(userId, dataUserDTO);
     }
 
     @DeleteMapping("/delete/{userId}")
-    public Boolean delete(@PathVariable Integer userId){
+    public Boolean delete(@PathVariable Integer userId) {
         return dataUserService.delete(userId);
     }
 }
--- src/main/java/com/technical_test/technical_test/service/DataUserService.java
+++ src/main/java/com/technical_test/technical_test/service/DataUserService.java
@@ -1,15 +1,54 @@
 package com.technical_test.technical_test.service;
 
 import com.technical_test.technical_test.dto.DataUserDTO;
-import com.technical_test.technical_test.model.DataUser;
+import com.technical_test.technical_test.repository.DataUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.Arrays;
 import java.util.List;
 
-public interface DataUserService {
-    List<DataUser> findAll();
-    DataUser findByUserId(Integer userId);
-    DataUser create(DataUser dataUser);
-    Boolean delete(Integer userId);
-    DataUserDTO mapToDto(DataUser dataUser);
-    DataUser mapToEntity(DataUserDTO dataUserDTO);
+import org.springframework.web.client.RestTemplate;
+
+
+@Service
+@Transactional
+public class DataUserService {
+
+    private final String HMDM_USER_API_URL = "http://localhost:8080/api/users";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+
+    @Autowired
+    DataUserRepository dataUserRepository;
+
+    public List<DataUserDTO> findAll() {
+        DataUserDTO[] users = restTemplate.getForObject(HMDM_USER_API_URL, DataUserDTO[].class);
+        return Arrays.asList(users);
+    }
+
+    public DataUserDTO findByUserId(Integer userId) {
+        String url = HMDM_USER_API_URL + "/" + userId;
+        return restTemplate.getForObject(url, DataUserDTO.class);
+    }
+
+    public DataUserDTO create(DataUserDTO dataUserDTO) {
+        return restTemplate.postForObject(HMDM_USER_API_URL, dataUserDTO, DataUserDTO.class);
+    }
+
+
+    public DataUserDTO update(Integer userId, DataUserDTO dataUserDTO) {
+        String url = HMDM_USER_API_URL + "/" + userId;
+        restTemplate.put(url, dataUserDTO);
+        return dataUserDTO;
+    }
+
+
+    public Boolean delete(Integer userId) {
+        String url = HMDM_USER_API_URL + "/" + userId;
+        restTemplate.delete(url);
+        return true;
+    }
+
 }
